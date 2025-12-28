60ed9c977bea9c2f4a72881c94398a7680d72516
Samad-Idrizaj <nastro3214@gmail.com>
2025-12-28 15:55:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,3 +15,25 @@ app.use(express.json());
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 });
+app.post("/chat", async (req, res) => {
+const completion = await openai.chat.completions.create({
+  model: "gpt-4.1-mini",
+  messages: [
+    { role: "user", content: req.body.message }
+  ]
+});
+res.json({
+  reply: completion.choices[0].message.content
+});
+app.post("/image", async (req, res) => {
+{ "prompt": "ein Cyberpunk Hund" }
+const img = await openai.images.generate({
+  model: "gpt-image-1",
+  prompt: req.body.prompt
+});
+res.json({
+  url: img.data[0].url
+});
+app.listen(3000, () => {
+  console.log("Server läuft");
+});
